4dd62745811ea1ff9ca88163edbf16e639b2f63c
VChorney <v56chorney95@gmail.com>
2019-01-24 22:12:58 +0200

    Empty

--- Calkulator/src/main/java/com/classes/Main.java
+++ Calkulator/src/main/java/com/classes/Main.java
@@ -1,18 +1,6 @@
-package com.classes;
-
-import com.forms.Form;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Form form=new Form();
-            form.setVisible(true);
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
+  
 
     }
 
--- Calkulator/src/main/java/com/classes/Vector.java
+++ Calkulator/src/main/java/com/classes/Vector.java
@@ -1,40 +1,4 @@
-package com.classes;
 
-public class Vector extends OrderedData<Double> {
-    public Vector(Double[][] firstMatrix, Double[][] secondMatrix, Double[][] resultMatrix) {
-        super(firstMatrix, secondMatrix, resultMatrix);
-    }
-
-    @Override
-    public Object[][] add(int i, int j, Object number, int choise) {
-        return super.add(i, j, number, choise);
-    }
-
-    @Override
-    public Object[][] plusAndMinus(int column, int row, Double[][] firstMatrix, Double[][] secondMatrix, boolean action) {
-        return super.plusAndMinus(column, row, firstMatrix, secondMatrix, action);
-    }
-
-    @Override
-    public Object[][] multiplicationByNumber(int column, int row, Double[][] matrix, double number) {
-        return super.multiplicationByNumber(column, row, matrix, number);
-    }
-
-    @Override
-    public Object[][] multiplication(int firstColumn, int firstRow, int secondColumn, int secondRow, Double[][] firstMatrix, Double[][] secondMatrix) {
-        setResultList(clean(getResultList()));
-        Double buffer=0.0;
-        for(int i=0;i<firstRow;i++){
-          buffer+=firstMatrix[i][0]*secondMatrix[i][0];
-        }
-        getResultList()[0][0]=buffer;
-        return getResultList();
-    }
-
-    @Override
-    public Object[][] transposing(int column, int row, Double[][] matrix) {
-        setFirstRow(1);
-        return super.transposing(column, row, matrix);
-
-    }
+public class Vector {
+    
 }
